e48b920b4bdd641dc87a8ba4b521257d53403b74
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-17 09:16:16 +0800

    Create Directory BajikApp
    Move splash screen and on boarding screen to BajikApp with separate file

---
+++ lib/BajikApp/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:bajik_app_monitoring_patient/BajikApp/on_boarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF5A5AFE),
+              Color(0xFF5FBBFD),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/images/logo.svg',
+                width: 219,
+                height: 116,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
